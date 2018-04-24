a02f4f39c5ea747c28d80677a79be653a1d00aef
IVAN SZOBOSZLAY JUNIOR <ivan.sjunior@senacsp.edu.br>
2018-04-24 11:20:59 -0300

    Estrutura e Variáveis

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculo_CEP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cpfcliente;
+            string cpf9;
+            string cpf10;
+            int contagem1=10, contagen2=10, resto=0, rs1=0, rs2=0;
+            
+
+        }
+    }
+}
